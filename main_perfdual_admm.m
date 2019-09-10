18d79bcbdfb00cf6b5fe63cd98ee075d8e499ad0
knayak <knayak@usc.edu>
2019-09-10 14:39:42 -0700

    Minor Fixes
    correction ifft3d -> ift3d
    Added comments

--- main_perfdual_admm.m
+++ main_perfdual_admm.m
@@ -1,26 +1,32 @@
+%% INITIALIZATION
+
 clear all; close all;
 
+% adding local subdirectories to the path (supporting functions)
 addpath('SKED');
 addpath('ADMM_functions');
 addpath('PerfDual_utils');
 addpath('sample_data');
-%%
-ncoil = 8;
-accel_ky = 3;
-accel_kz = 2;
 
-flag_viewshare = 1;
-lambda = 0.1;
+%% IMAGING PARAMETERS
+ncoil = 8;    % number of receiver coils
+accel_ky = 3; % undersampling factor along k_y
+accel_kz = 2; % undersampling factor along k_z
 
-%%
-[dr,rhfrsize,usercv,rhnframes,rhnslices,hdr] = rawloadHD_si('P39936.7');
+flag_viewshare = 1;  % perform view-sharing (borrow data from temporally adjacent time points)
+lambda = 0.1;        % regularization parameter (how much to weight "total variation sparsity")
 
-[kd,idxd, ks,idxs, usercv] = sampling(dr,rhfrsize,usercv,rhnframes,rhnslices,ncoil, accel_ky, accel_kz);
+%% LOAD DATA AND SETUP DATA STRUCTURE
 
-if flag_viewshare ==1 % For quick look
+[dr,rhfrsize,usercv,rhnframes,rhnslices,hdr] = rawloadHD_si('P39936.7');  % loads GE Raw Data File (a.k.a. "P-file")
+
+[kd,idxd,ks,idxs,usercv] = sampling(dr,rhfrsize,usercv,rhnframes,rhnslices,ncoil,accel_ky,accel_kz); % generates the ky,kz,t sample locations
+
+if flag_viewshare == 1 % For quick look (no reconstruction constraints, view sharing)
     [kd_vs, ~, ks_vs, ~] = viewsharing(kd, idxd, ks, idxs, accel_ky*accel_kz);
+    
     % Diastole
-    imgR_D = ifft3d(kd_vs);
+    imgR_D = ift3d(kd_vs);
     imgR_D = sum(imgR_D.*conj(imgR_D), 5);
     imgR_D = imresize_perf(imgR_D, usercv, 1); 
     im1 = cat(2, imgR_D(:,:,1,:), imgR_D(:,:,2,:), imgR_D(:,:,3,:), imgR_D(:,:,4,:), imgR_D(:,:,5,:));
@@ -31,7 +37,7 @@ if flag_viewshare ==1 % For quick look
     img2mp4( abs(im), 10,'Diastole_viewsharing.mp4');
     
     % Systole
-    imgR_S = ifft3d(ks_vs);
+    imgR_S = ift3d(ks_vs);
     imgR_S = sum(imgR_S.*conj(imgR_S), 5);
     imgR_S = imresize_perf(imgR_S,usercv,0);
     im1s = cat(2, imgR_S(:,:,1,:), imgR_S(:,:,2,:), imgR_S(:,:,3,:), imgR_S(:,:,4,:));
@@ -49,7 +55,7 @@ clear imgR_D imgR_S im ims;
 
 imgR_D = TV_recon(kd,idxd,sMaps,lambda);
 
-imgR_D = imresize_perf(imgR_D,usercv,1);%diastole
+imgR_D = imresize_perf(imgR_D,usercv,1); %diastole
 
 im1 = cat(2, imgR_D(:,:,1,:), imgR_D(:,:,2,:), imgR_D(:,:,3,:), imgR_D(:,:,4,:), imgR_D(:,:,5,:));
 im2 = cat(2, imgR_D(:,:,6,:), imgR_D(:,:,7,:), imgR_D(:,:,8,:), imgR_D(:,:,9,:), imgR_D(:,:,10,:));
